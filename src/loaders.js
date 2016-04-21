68f5298be9d2adc41816c2bfd9e4bd95ab1f1fb2
Jake Hiller <jakemhiller@gmail.com>
2016-04-21 15:03:42 -0400

    just use smaller classnames all of the time

--- src/loaders.js
+++ src/loaders.js
@@ -4,12 +4,11 @@ import ExtractTextPlugin from 'extract-text-webpack-plugin';
 const CSS_CLIENT_PREFIX = '!css';
 const CSS_SERVER_PREFIX = 'css/locals';
 
-const SCSS_OPTIONS = '?-minimize&sourceMap&modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!sass?sourceMap';
-const SCSS_OPTIONS_SERVER = '?-minimize&sourceMap&modules&importLoaders=1!sass?sourceMap';
+const SCSS_OPTIONS = '?-minimize&sourceMap&modules&importLoaders=1!sass?sourceMap';
 const CSS_OPTIONS = '?-minimize&sourceMap';
 
 const SCSS_CLIENT = `${CSS_CLIENT_PREFIX}${SCSS_OPTIONS}`;
-const SCSS_SERVER = `${CSS_SERVER_PREFIX}${SCSS_OPTIONS_SERVER}`;
+const SCSS_SERVER = `${CSS_SERVER_PREFIX}${SCSS_OPTIONS}`;
 
 const CSS_CLIENT = `${CSS_CLIENT_PREFIX}${CSS_OPTIONS}`;
 const CSS_SERVER = `${CSS_SERVER_PREFIX}${CSS_OPTIONS}`;
