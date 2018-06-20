c5029533b572826a22acbaeee85c24aadc2a30f8
Jake Hiller <jakemhiller@gmail.com>
2018-06-20 00:32:06 -0400

    WIP

---
+++ packages/webpack-config/razzle.js
@@ -0,0 +1,23 @@
+const merge = require('webpack-merge');
+const configs = require('./src/config');
+
+const modify = (initialConfig, { target, dev }, webpack) => {
+  let config = initialConfig;
+  // Change the name of the server output file in production
+  if (target === 'node') {
+    config = merge(config, configs.cssServer());
+  } else if (dev) {
+    config = merge(config, configs.css());
+  } else {
+    config = merge(
+      config,
+      configs.cssExtracted({
+        filename: '[name]-[contenthash].css',
+      })
+    );
+  }
+  console.log(config);
+  return config;
+};
+
+module.exports = modify;
